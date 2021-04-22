b305bb157659c4c6835e699619937c1eb44f23df
sorikikikim <sorikim98@naver.com>
2021-04-22 16:29:52 +0900

    ft_server

--- srcs/run.sh
+++ srcs/run.sh
@@ -16,7 +16,7 @@ mv wordpress/ var/www/html/
 cp -rp /tmp/wp-config.php var/www/html/wordpress
 
 service mysql start
-mysql -u root --skip-password < init.sql
+mysql -u root < init.sql
 
 wget https://files.phpmyadmin.net/phpMyAdmin/5.0.2/phpMyAdmin-5.0.2-all-languages.tar.gz
 tar -xvf phpMyAdmin-5.0.2-all-languages.tar.gz
@@ -24,7 +24,6 @@ mv phpMyAdmin-5.0.2-all-languages phpmyadmin
 mv phpmyadmin /var/www/html/
 rm phpMyAdmin-5.0.2-all-languages.tar.gz
 cp -rp /tmp/config.inc.php /var/www/html/phpmyadmin/
-mysql < var/www/html/phpmyadmin/sql/create_tables.sql
 
 service nginx start
 service php7.3-fpm start
